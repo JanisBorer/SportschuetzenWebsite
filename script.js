4ef1d8e68a27a0f512e6442109165ab4240a6ae9
Janis <258025719+JanisBorer@users.noreply.github.com>
2026-02-03 13:34:07 +0100

    alles

---
+++ script.js
@@ -0,0 +1,40 @@
+<<<<<<< HEAD
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
+
+window.onload = function() {
+    var param = getUrlParameter('div');
+    var divs = document.querySelectorAll('div[id^="unique-div"]');
+
+    divs.forEach(function(div) {
+        if (div.id === 'unique-div' + param) {
+            div.classList.remove('hidden');
+        } else {
+            div.classList.add('hidden');
+        }
+    });
+=======
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
+
+window.onload = function() {
+    var param = getUrlParameter('div');
+    var divs = document.querySelectorAll('div[id^="unique-div"]');
+
+    divs.forEach(function(div) {
+        if (div.id === 'unique-div' + param) {
+            div.classList.remove('hidden');
+        } else {
+            div.classList.add('hidden');
+        }
+    });
+>>>>>>> c71a260 (Erster Commit$)
+};
