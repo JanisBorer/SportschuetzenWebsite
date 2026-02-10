0e9e46ed7c4a649702264f445d6bcd10dd611043
Janis <258025719+JanisBorer@users.noreply.github.com>
2026-02-10 16:29:05 +0100

    Neuste änderungen

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 function getUrlParameter(name) {
     name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
     var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
@@ -17,24 +17,4 @@ window.onload = function() {
             div.classList.add('hidden');
         }
     });
-=======
-function getUrlParameter(name) {
-    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-    var results = regex.exec(location.search);
-    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-}
-
-window.onload = function() {
-    var param = getUrlParameter('div');
-    var divs = document.querySelectorAll('div[id^="unique-div"]');
-
-    divs.forEach(function(div) {
-        if (div.id === 'unique-div' + param) {
-            div.classList.remove('hidden');
-        } else {
-            div.classList.add('hidden');
-        }
-    });
->>>>>>> c71a260 (Erster Commit$)
 };
